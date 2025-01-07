b493127d65c30aab294409d37a4008333df78454
Mohsin0786 <mk67205@gmail.com>
2025-01-07 15:50:10 +0530

    changed health route

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Server is Healthy!!!!');
 });
 
